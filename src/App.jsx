4413b7b724bce31b13fab4853e91877b70738e16
abirsinha88 <abirsinha88@gmail.com>
2025-08-27 21:36:07 +0530

    Header logo added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-
+import Home from './Home'
+import {BrowserRouter, Routes, Route,Link} from 'react-router-dom'
 
 
 function App() {
 
 
   return (
+     <BrowserRouter>
+     <header>
+      <div className='header-container'>
+        <div className="header-logo-container">
+           <Link to="/"> 
+        <img src="../public/icon.png" alt="Logo" />
+        </Link>
+        </div>
+      </div>
+     </header>
+  <Routes >
+    <Route path="/" element = {<Home />}/>
+  </Routes>
+  </BrowserRouter>
     
-    <h1>Hello World</h1>
   
   )
 }
