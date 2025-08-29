88ba604db7c51c8c601ed1f15052926d154a83e9
abirsinha88 <abirsinha88@gmail.com>
2025-08-29 13:58:17 +0530

    caroussel button fixed

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,23 +1,20 @@
-import { useState } from "react";
-import { FaArrowAltCircleLeft } from "react-icons/fa";import { FaArrowAltCircleRight } from "react-icons/fa";
-
+import carousel1 from "../public/carousel-1.jpg";
+import carousel2 from "../public/carousel-2.jpg";
+import carousel3 from "../public/carousel-3.jpg";
+import ImageSlider from "./ImageSlider.jsx"
 
 function Home(){
-    const [carouselImage, SetCaraouselImage] = useState(2);
+  const images=[carousel1,carousel2,carousel3];
    
 
 
     return (
-        <section className="carousel-image">
-             <div className="image-container">
-            <img src={`/carousel-${carouselImage}.jpg`}
-            className="carousal"  alt="" />
-
-          <FaArrowAltCircleLeft style={{left:"20px"}}/>
-            <FaArrowAltCircleRight style={{right:"20px"}} />
-
+     
+        <div style={{maxWidth:"1200px", width:"100%",height:"300px",
+            margin: "0px auto"
+       }}>
+            <ImageSlider imageurls={{images}}/>
         </div>
-        </section>
     )
         
        
