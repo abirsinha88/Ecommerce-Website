4e98a6a71fbd1120a0bc68d59dc2ba7bc33247b2
abirsinha88 <abirsinha88@gmail.com>
2025-10-16 20:58:30 +0530

    shadcn/ui configured

--- src/ProductDetails.jsx
+++ src/ProductDetails.jsx
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom";
 import { useState } from "react";
 import { useEffect } from "react";
+import { Button } from "@/components/ui/button"
+
 function ProductDetails(){
     const [vans,setVans] = useState([]);
     const params= useParams();
@@ -12,7 +14,13 @@ function ProductDetails(){
     return( 
         <>
          <h1>product detail</h1>
-        `<div>{vans.description}</div>
+          <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <div className="flex flex-wrap justify-center gap-2 md:flex-row">
+      <Button Button variant="link" >Button</Button>
+    </div>
+        <div>{vans.description}</div>
         </>  
    
 )
